f9b7e8591c825452ac8e8f584aee584ceab57fb4
tylerJtucker <tyletuck418@ed.amdsb.ca>
2017-11-08 10:12:31 -0800

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redButton = new System.Windows.Forms.PictureBox();
+            this.blueButton = new System.Windows.Forms.PictureBox();
+            this.m = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.Label();
+            this.redOption = new System.Windows.Forms.Label();
+            this.blueOption = new System.Windows.Forms.Label();
+            this.yellowOption = new System.Windows.Forms.Label();
+            this.yellowButton = new System.Windows.Forms.PictureBox();
+            this.c = new System.Windows.Forms.Label();
+            this.wPicture = new System.Windows.Forms.PictureBox();
+            this.lPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.redButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(350, 240);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(799, 547);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Transparent;
+            this.redButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redButton.BackgroundImage")));
+            this.redButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redButton.ErrorImage = null;
+            this.redButton.Location = new System.Drawing.Point(81, 518);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(119, 114);
+            this.redButton.TabIndex = 1;
+            this.redButton.TabStop = false;
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Transparent;
+            this.blueButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("blueButton.BackgroundImage")));
+            this.blueButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.blueButton.ErrorImage = null;
+            this.blueButton.Location = new System.Drawing.Point(81, 657);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(107, 98);
+            this.blueButton.TabIndex = 2;
+            this.blueButton.TabStop = false;
+            // 
+            // m
+            // 
+            this.m.BackColor = System.Drawing.Color.Transparent;
+            this.m.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m.ForeColor = System.Drawing.Color.White;
+            this.m.Location = new System.Drawing.Point(48, 547);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(27, 33);
+            this.m.TabIndex = 3;
+            this.m.Text = "M";
+            // 
+            // b
+            // 
+            this.b.AutoSize = true;
+            this.b.BackColor = System.Drawing.Color.Transparent;
+            this.b.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.ForeColor = System.Drawing.Color.White;
+            this.b.Location = new System.Drawing.Point(48, 680);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(23, 23);
+            this.b.TabIndex = 4;
+            this.b.Text = "B";
+            // 
+            // redOption
+            // 
+            this.redOption.BackColor = System.Drawing.Color.Transparent;
+            this.redOption.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redOption.ForeColor = System.Drawing.Color.White;
+            this.redOption.Location = new System.Drawing.Point(239, 567);
+            this.redOption.Name = "redOption";
+            this.redOption.Size = new System.Drawing.Size(693, 33);
+            this.redOption.TabIndex = 5;
+            this.redOption.Text = "Red Option";
+            // 
+            // blueOption
+            // 
+            this.blueOption.BackColor = System.Drawing.Color.Transparent;
+            this.blueOption.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueOption.ForeColor = System.Drawing.Color.White;
+            this.blueOption.Location = new System.Drawing.Point(239, 689);
+            this.blueOption.Name = "blueOption";
+            this.blueOption.Size = new System.Drawing.Size(1092, 33);
+            this.blueOption.TabIndex = 6;
+            this.blueOption.Text = "Blue Option";
+            // 
+            // yellowOption
+            // 
+            this.yellowOption.BackColor = System.Drawing.Color.Transparent;
+            this.yellowOption.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowOption.ForeColor = System.Drawing.Color.White;
+            this.yellowOption.Location = new System.Drawing.Point(1125, 560);
+            this.yellowOption.Name = "yellowOption";
+            this.yellowOption.Size = new System.Drawing.Size(272, 33);
+            this.yellowOption.TabIndex = 7;
+            this.yellowOption.Text = "Yellow Option";
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Transparent;
+            this.yellowButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("yellowButton.BackgroundImage")));
+            this.yellowButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.yellowButton.ErrorImage = null;
+            this.yellowButton.Location = new System.Drawing.Point(985, 518);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(119, 114);
+            this.yellowButton.TabIndex = 8;
+            this.yellowButton.TabStop = false;
+            // 
+            // c
+            // 
+            this.c.AutoSize = true;
+            this.c.BackColor = System.Drawing.Color.Transparent;
+            this.c.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.ForeColor = System.Drawing.Color.White;
+            this.c.Location = new System.Drawing.Point(938, 567);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(23, 23);
+            this.c.TabIndex = 9;
+            this.c.Text = "C";
+            // 
+            // wPicture
+            // 
+            this.wPicture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("wPicture.BackgroundImage")));
+            this.wPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.wPicture.ErrorImage = null;
+            this.wPicture.InitialImage = null;
+            this.wPicture.Location = new System.Drawing.Point(65, 216);
+            this.wPicture.Name = "wPicture";
+            this.wPicture.Size = new System.Drawing.Size(256, 224);
+            this.wPicture.TabIndex = 10;
+            this.wPicture.TabStop = false;
+            // 
+            // lPicture
+            // 
+            this.lPicture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("lPicture.BackgroundImage")));
+            this.lPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.lPicture.ErrorImage = null;
+            this.lPicture.InitialImage = null;
+            this.lPicture.Location = new System.Drawing.Point(65, 216);
+            this.lPicture.Name = "lPicture";
+            this.lPicture.Size = new System.Drawing.Size(256, 224);
+            this.lPicture.TabIndex = 11;
+            this.lPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1428, 865);
+            this.Controls.Add(this.lPicture);
+            this.Controls.Add(this.wPicture);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.yellowButton);
+            this.Controls.Add(this.yellowOption);
+            this.Controls.Add(this.blueOption);
+            this.Controls.Add(this.redOption);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.m);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Run for President";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.redButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox redButton;
+        private System.Windows.Forms.PictureBox blueButton;
+        private System.Windows.Forms.Label m;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.Label redOption;
+        private System.Windows.Forms.Label blueOption;
+        private System.Windows.Forms.Label yellowOption;
+        private System.Windows.Forms.PictureBox yellowButton;
+        private System.Windows.Forms.Label c;
+        private System.Windows.Forms.PictureBox wPicture;
+        private System.Windows.Forms.PictureBox lPicture;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,300 @@
+﻿//Tyler tucker
+//a choose your own adventure text game, were the user is running for president
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int Scene = 0;   //variables
+        int delay = 370; //Fascism > Communism
+        int percentage;
+
+        Random ranGen = new Random(); //random number gen
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel.Text = "You are literally any US citzen ever, would you like to run for president?";
+            redOption.Text = "Yes";
+            blueOption.Text = "No";
+            Refresh();
+            yellowOption.Visible = false;
+            yellowButton.Visible = false;
+            c.Visible = false;
+            lPicture.Visible = false;
+            wPicture.Visible = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            SoundPlayer button = new SoundPlayer(Properties.Resources.Ding);  //makes sound players
+            SoundPlayer Win = new SoundPlayer(Properties.Resources.Win);
+            SoundPlayer Lose = new SoundPlayer(Properties.Resources.lose);
+
+            if (e.KeyCode == Keys.M)       //red button press and what will happen
+            {
+                button.Play();
+
+                if (Scene == 0) { Scene = 1; }
+                else if (Scene == 1)
+                {
+                    Scene = 3;
+                    blueButton.Visible = true;
+                    blueOption.Visible = true;
+                }
+                else if (Scene == 3) { Scene = 5; }
+                else if (Scene == 5) { Scene = 7; }
+                else if (Scene == 6) { Scene = 8; }
+                else if (Scene == 8) { Scene = 18; }
+                else if (Scene == 9) { Scene = 12; }
+                else if (Scene == 12)
+                {
+                    Scene = 13;
+                    yellowButton.Visible = false;
+                    yellowOption.Visible = false;
+                    c.Visible = false;
+                }
+
+                else if (Scene == 14) { Scene = 20; }
+                else if (Scene == 13) { Scene = 16; }
+                else if (Scene == 21) { Scene = 0; }
+                else if (Scene == 22) { Scene = 0; }
+            }
+
+            else if (e.KeyCode == Keys.B)  //blue button press and what will happen when it is pressed
+            {
+                button.Play();
+
+                if (Scene == 0) { Scene = 2; }
+                else if (Scene == 1)
+                {
+                    blueButton.Visible = false;
+                    blueOption.Visible = false;
+                }
+                else if (Scene == 3) { Scene = 4; }
+                else if (Scene == 4) { Scene = 21; }
+                else if (Scene == 5) { Scene = 6; }
+                else if (Scene == 6) { Scene = 9; }
+                else if (Scene == 9) { Scene = 10; }
+                else if (Scene == 12)
+                {
+                    Scene = 14;
+                    yellowButton.Visible = false;
+                    yellowOption.Visible = false;
+                    c.Visible = false;
+                }
+                else if (Scene == 14) { Scene = 21; }
+                else if (Scene == 8) { Scene = 19; }
+                else if (Scene == 21) Close();
+                else if (Scene == 22) Close();
+            }
+
+            else if (e.KeyCode == Keys.C)  //yellow button press and what will happen when it is pushed
+            {
+                if (Scene == 12)
+                {
+                    button.Play();
+
+                    Scene = 15;
+                    yellowOption.Visible = false;
+                    yellowButton.Visible = false;
+                    c.Visible = false;
+                }
+            }
+
+            else if (e.KeyCode == Keys.L)  //to get to play again screen user presses L
+            {
+                if (Scene == 7 || Scene == 2 || Scene == 4 || Scene == 10 || Scene == 18 || Scene == 16 || Scene == 17 || Scene == 20) { Scene = 21; }
+            }
+            else if (e.KeyCode == Keys.W)  //to get to play again screen user presses W
+            {
+                if (Scene == 15 || Scene == 19 || Scene == 15) { Scene = 22; }
+            }
+
+            switch (Scene) //all scenes can what options that are presented to the user
+            {
+                case 0:
+                    lPicture.Visible = false;
+                    wPicture.Visible = false;
+                    outputLabel.Text = "You are literally any US citzen ever, would you like to run for president?";
+                    redOption.Text = "Yes";
+                    blueOption.Text = "No";
+                    Refresh();
+                    break;
+                case 1:
+                    Thread.Sleep(delay);
+                    outputLabel.Text = "You're in!! its time to choose your party, would you like to be a Communist or a Facist?";
+                    redOption.Text = "Communist";
+                    blueOption.Text = "Fascist";
+                    Refresh();
+                    break;
+                case 2:
+                    outputLabel.Text = "You continued to live up to nothing in life, your parents are disappointed\ and you die alone....You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 3:
+                    outputLabel.Text = "Now that you have chosen your party its time for your first press conference. Would you like to have it November 12th or January 3rd?";
+                    redOption.Text = "January 3rd";
+                    blueOption.Text = "Novemeber 12th";
+                    Refresh();
+                    break;
+                case 4:
+                    outputLabel.Text = "The fascists came to your press conference and assasinated you... You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 5:
+                    outputLabel.Text = "Your press conference was a success, voters love the communist party. Choose a country to bribe so you can win the election. Iran or Russia?";
+                    redOption.Text = "Russia";
+                    blueOption.Text = "Iran";
+                    Refresh();
+                    break;
+                case 6:
+                    outputLabel.Text = "Iran promises you all there oil and you tell the public";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "Once you win the election Iran does not keep its promise, will you go to war with Iran?";
+                    redOption.Text = "Yes";
+                    blueOption.Text = "No";
+                    Refresh();
+                    break;
+                case 7:
+                    outputLabel.Text = "Stalin awakes from the dead, and kills you, You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 8:
+                    outputLabel.Text = "You attack Iran";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    outputLabel.Text = "The war goes on for 6 months, Iran wants to sign a peace treaty. Will you sign? or will you demand all there oil?";
+                    redOption.Text = "Give up";
+                    blueOption.Text = "Demand Oil";
+                    Refresh();
+                    break;
+                case 9:
+                    outputLabel.Text = "Three months later Iran donates all its oil to russia. Now will you go to war with Iran?";
+                    redOption.Text = "Yes";
+                    blueOption.Text = "No";
+                    Refresh();
+                    break;
+                case 10:
+                    outputLabel.Text = "Iran invades first, your country is mad at you and assasinates you....You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 12:
+                    percentage = ranGen.Next(1, 101);
+                    if (percentage > 30)
+                    {
+                        outputLabel.Text = "Justin Trudeu comes to the US for a meeting. What do you tell him?";
+                        redOption.Text = "You are going to invade cuz you salty";
+                        blueOption.Text = "You lie and say you want to change the national anthem";
+                        yellowOption.Text = "Invade canada insted";
+                        yellowButton.Visible = true;
+                        yellowOption.Visible = true;
+                        c.Visible = true;
+                        Refresh();
+                    }
+                    else
+                    {
+                        outputLabel.Text = "You attack Iran, and take all there oil, Canada then attacks you to get the oil and ww3 breaks out....You Lose. Press L to take the L";
+                        redOption.Text = "";
+                        blueOption.Text = "";
+                        Refresh();
+                    }
+                    break;
+                case 13:
+                    outputLabel.Text = "Justin Trudeau joins the war efforts, but you need more Country to invade Iran. Will you recruit Switzerland or Germany?";
+                    redOption.Text = "Switzerland";
+                    blueOption.Text = "Germany";
+                    Refresh();
+                    break;
+                case 14:
+                    outputLabel.Text = "Justin Trudeau thinks you wasted his time and leaves. Do you try and stop him?";
+                    redOption.Text = "Oui";
+                    blueOption.Text = "Non";
+                    Refresh();
+                    break;
+                case 15:
+                    outputLabel.Text = "Canada says sorry and gives you all their oil...You win. Press W to take the W";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 16:
+                    outputLabel.Text = "Switzerland is neutral, you attack anyway and lose due to a shortage of soldiers.....You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 17:
+                    outputLabel.Text = "Germany is still salty about the treaty of versify they nuke the US.... You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 18:
+                    outputLabel.Text = "You failed this country...You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 19:
+                    outputLabel.Text = "Iran can't fight any longer, they give in and give you all the oil...You Win. Press W to take the W.";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 20:
+                    outputLabel.Text = "He leaves anyway...You Lose. Press L to take the L";
+                    redOption.Text = "";
+                    blueOption.Text = "";
+                    Refresh();
+                    break;
+                case 21:
+                    lPicture.Visible = true;
+                    outputLabel.Text = "YOU TOOK THE L...Do you want to play again?";
+                    redOption.Text = "Yes";
+                    blueOption.Text = "No";
+                    Lose.Play();
+                    Refresh();
+                    break;
+                case 22:
+                    wPicture.Visible = true;
+                    outputLabel.Text = "YOU TOOK THE W...Do you want to play again?";
+                    redOption.Text = "Yes";
+                    blueOption.Text = "No";
+                    Win.Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
